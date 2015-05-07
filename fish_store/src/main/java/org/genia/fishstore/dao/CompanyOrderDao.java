3b39c5ec87efdec8f907dd5d22d72dcaebc2de02
wayfarer <genia.sushko@gmail.com>
2015-05-08 00:28:30 +0300

    Some changes

--- fish_store/src/main/java/org/genia/fishstore/dao/CompanyOrderDao.java
+++ fish_store/src/main/java/org/genia/fishstore/dao/CompanyOrderDao.java
@@ -7,10 +7,10 @@ import org.genia.fishstore.entities.CompanyOrderItem;
 
 public interface CompanyOrderDao extends GenericDao<CompanyOrder> {
 	public List<CompanyOrderItem> getAvailableItems();
-	public void putOnSale(CompanyOrderItem item);			//mark as available for sale
-	public void removeFromSale(CompanyOrderItem item);		//mark as not available for sale
+	//public void putOnSale(CompanyOrderItem item);			//mark as available for sale
+	//public void removeFromSale(CompanyOrderItem item);		//mark as not available for sale
 	public void acceptOnStore(CompanyOrderItem item);		//accept order item to store as is.
-	public void writeOffOrderItem(CompanyOrderItem item);	//write-off the rest
+	//public void writeOffOrderItem(CompanyOrderItem item);	//write-off the rest
 	public String generateReportByDates();
 	public String generateReportByFishTypes();
 }
--- fish_store/src/main/java/org/genia/fishstore/dao/EmployeeDao.java
+++ fish_store/src/main/java/org/genia/fishstore/dao/EmployeeDao.java
@@ -1,9 +1,11 @@
 package org.genia.fishstore.dao;
 
+import java.util.List;
+
 import org.genia.fishstore.entities.Employee;
 
 public interface EmployeeDao extends GenericDao<Employee> {
 	public Employee findByLogin(String login);
 	public void suspendById(int id);
-
+	public List<Employee> getEmployeeList();
 }
