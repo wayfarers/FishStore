f04a6c4e2235e965410b84bf6f94b63c2b1fc414
wayfarer <genia.sushko@gmail.com>
2015-06-16 16:53:29 +0300

    Added page for accountant.

--- fish_store/src/main/java/org/genia/fishstore/web/AccountantBean.java
+++ fish_store/src/main/java/org/genia/fishstore/web/AccountantBean.java
@@ -7,7 +7,9 @@ import javax.inject.Named;
 
 import org.genia.fishstore.CustomerOrderFilter;
 import org.genia.fishstore.entities.CustomerOrder;
+import org.genia.fishstore.entities.OrderStatus;
 import org.genia.fishstore.services.CustomerOrderService;
+import org.primefaces.context.RequestContext;
 import org.springframework.context.annotation.Scope;
 
 @Named
@@ -16,10 +18,37 @@ public class AccountantBean {
 	
 	@Inject
 	CustomerOrderService customerOrderService;
+	@Inject
+	SessionDataBean sessionData;
+	
+	private CustomerOrder selectedOrder;
+	
+	public AccountantBean() {
+		selectedOrder = new CustomerOrder();
+	}
 	
 	CustomerOrderFilter filter = new CustomerOrderFilter();
-	public List<CustomerOrder> getCustomerOrders(CustomerOrderFilter filter) {
-		filter.setUnPayed(true);
+	public List<CustomerOrder> getUnpaidOrders() {
+//		filter.setUnPayed(true);
 		return customerOrderService.findByFilter(filter).getResultList();
+	}
+	
+	public CustomerOrder getSelectedOrder() {
+		return selectedOrder;
+	}
+	public void setSelectedOrder(CustomerOrder selectedOrder) {
+		this.selectedOrder = selectedOrder;
+		RequestContext.getCurrentInstance().execute("PF('edit_dlg').show()");
+	}
+	
+	public void saveOrder() {
+		customerOrderService.save(selectedOrder);
+		RequestContext.getCurrentInstance().execute("PF('edit_dlg').hide()");
+	}
+	
+	public void approve(CustomerOrder order) {
+		order.setAccountant(sessionData.getLoggedInEmployee());
+		order.setStatus(OrderStatus.ACCEPTED);
+		customerOrderService.save(order);
 	}
 }
